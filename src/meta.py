47c0aba1ca093e8eade25962cd01b099bedfe7d6
Tasqu <topi.rasku@vtt.fi>
2024-04-12 12:37:29 +0300

    Bump version

--- src/meta.py
+++ src/meta.py
@@ -1,5 +1,4 @@
 """ArBuWe metadata"""
-__version__ = "1.0.2"
-description = (
-    "A python package for processing and aggregating building stock heating/cooling demand and weather data for `ArBuMo.jl`"
-)
+
+__version__ = "2.0.0"
+description = "A python package for processing and aggregating building stock heating/cooling demand and weather data for `ArBuMo.jl`"
